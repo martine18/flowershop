5b95ec842b6fcb2f1d6476a7dd1699e1c764fc90
martine18 <the7sevenstar@gmail.com>
2023-02-03 21:56:11 -0800

    Done

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-function changeImage() {
-    var image = document.getElementById('myImage');
-    if (image.src.match("bulbon")) {
-        image.src = "./corina-ardeleanu-sWlxCweDzzs-unsplash.jpg";
-    } else {
-        image.src = "./angelina-jollivet-mNEpmNiFdXs-unsplash.jpg";
-    }
-      
-}
-
-
-
-
 
 
 
